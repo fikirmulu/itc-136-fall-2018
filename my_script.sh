416be7b2e061415be351c225be9fd19bccb4a42f
fikirte mulugeta <fikirliya@gmail.com>
2018-10-29 21:04:03 -0700

    I add more stuff per Nicole said so

--- my_script.sh
+++ my_script.sh
@@ -4,3 +4,12 @@ echo "hello ITC-136"
 echo "Edited in github"
 
 
+echo "This is your current working directory: "
+pwd
+
+echo "This is the version of linux you're running: "
+uname -a
+
+echo "These are your logged in users:"
+who
+
